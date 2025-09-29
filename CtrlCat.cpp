8da0dfb244a7886312d1fddc508398ac1cb1b656
Abdelrahman Ahmed <u24898008@tuks.co.za>
2025-09-29 16:09:55 +0200

    Update CtrlCat.cpp

--- CtrlCat.cpp
+++ CtrlCat.cpp
@@ -13,7 +13,7 @@ void CtrlCat::registerUser(User* user) {
     std::vector<ChatRoom*> chatRooms = user->getChatRooms();
     chatRooms.push_back(this);
     user->setChatRooms(chatRooms);
-    notify("New user joined", this);
+    notify("user_joined", this);
 }
 
 void CtrlCat::removeUser(User* user) {
@@ -21,7 +21,7 @@ void CtrlCat::removeUser(User* user) {
     for(int i = 0; i < users.size(); i++) {
         if(users[i] == user) {
             users.erase(users.begin() + i);
-            notify("some user left", this);
+            notify("user_left", this);
             break;
         }
     }
@@ -32,7 +32,7 @@ void CtrlCat::sendMessage(std::string message, User *fromUser) {
     std::vector<User*> users = getUsers();
     for(int i = 0; i < users.size(); i++) users[i]->receive(message, fromUser, this);
     saveMessage(message, fromUser);
-    notify("New message sent", this);
+    notify("message_sent", this);
 }
 
 void CtrlCat::saveMessage(std::string message, User *fromUser) {
