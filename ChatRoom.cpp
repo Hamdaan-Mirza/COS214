79038fde9ddfecf5f7119daf4e4a898a3119ebb6
Abdelrahman Habish <u24898008@tuks.co.za>
2025-09-29 12:04:36 +0200

    Added Creation Methods for Iterators

--- ChatRoom.cpp
+++ ChatRoom.cpp
@@ -3,24 +3,38 @@
 
 ChatRoom::ChatRoom() {}
 
-ChatRoom::~ChatRoom() {
-    for(int i = 0; i < chatHistory.size(); i++) {
+ChatRoom::~ChatRoom()
+{
+    for (int i = 0; i < chatHistory.size(); i++)
+    {
         delete chatHistory[i];
     }
     chatHistory.clear();
     users.clear();
 }
 
-std::vector<std::string*> ChatRoom::getChatHistory() { return chatHistory; }
+Iterator<User *> *ChatRoom::createUserIterator()
+{
+    return new ChatRoomIterator<User *>(users);
+}
 
-std::vector<User*> ChatRoom::getUsers() { return users; }
+Iterator<std::string *> *ChatRoom::createMessageIterator()
+{
+    return new ChatRoomIterator<std::string *>(chatHistory);
+}
 
-std::vector<User*> ChatRoom::setUsers(std::vector<User*> users) {
+std::vector<std::string *> ChatRoom::getChatHistory() { return chatHistory; }
+
+std::vector<User *> ChatRoom::getUsers() { return users; }
+
+std::vector<User *> ChatRoom::setUsers(std::vector<User *> users)
+{
     this->users = users;
     return this->users;
 }
 
-std::vector<std::string*> ChatRoom::setChatHistory(std::vector<std::string*> chatHistory) {
+std::vector<std::string *> ChatRoom::setChatHistory(std::vector<std::string *> chatHistory)
+{
     this->chatHistory = chatHistory;
     return this->chatHistory;
 }
--- ChatRoom.h
+++ ChatRoom.h
@@ -1,6 +1,7 @@
 #ifndef CHATROOM_H
 #define CHATROOM_H
 
+#include "ChatRoomIterator.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,21 +10,25 @@
 
 class User;
 
-class ChatRoom {
-    private:
-        std::vector<User*> users;
-        std::vector<std::string*> chatHistory;
-    public:
-        ChatRoom();
-        virtual ~ChatRoom();
-        virtual void registerUser(User* user) = 0;
-        virtual void removeUser(User* user) = 0;
-        virtual void sendMessage(std::string message, User *fromUser) = 0;
-        virtual void saveMessage(std::string message, User *fromUser) = 0;
-        std::vector<std::string*> getChatHistory();
-        std::vector<User*> getUsers();
-        std::vector<User*> setUsers(std::vector<User*> users);
-        std::vector<std::string*> setChatHistory(std::vector<std::string*> chatHistory);
+class ChatRoom
+{
+private:
+    std::vector<User *> users;
+    std::vector<std::string *> chatHistory;
+
+public:
+    ChatRoom();
+    virtual ~ChatRoom();
+    virtual void registerUser(User *user) = 0;
+    virtual void removeUser(User *user) = 0;
+    virtual void sendMessage(std::string message, User *fromUser) = 0;
+    virtual void saveMessage(std::string message, User *fromUser) = 0;
+    virtual Iterator<User *> *createUserIterator();
+    virtual Iterator<std::string *> *createMessageIterator();
+    std::vector<std::string *> getChatHistory();
+    std::vector<User *> getUsers();
+    std::vector<User *> setUsers(std::vector<User *> users);
+    std::vector<std::string *> setChatHistory(std::vector<std::string *> chatHistory);
 };
 
 #endif
